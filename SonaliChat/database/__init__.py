dcd49c2991474382cdc050d8a2ff7b9e8882342c
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-05-07 02:46:46 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- SonaliChat/database/__init__.py
+++ SonaliChat/database/__init__.py
@@ -2,12 +2,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import config
 
 # Database connection
-vickdb = AsyncIOMotorClient(config.MONGO_URL)
-vick = vickdb["VickDb"]["Vick"]
-usersdb = vick["users"]    # Users Collection
-chatsdb = vick["chats"]    # Chats Collection
+ChatBot = AsyncIOMotorClient(config.MONGO_URL)
+db = ChatBot["ChatBot"]  # Database
+usersdb = db["users"]    # Users Collection
+chatsdb = db["chats"]    # Chats Collection
 
 # Import functions for use in other parts of the application
 from .chats import *
-from .admin import *
 from .fsub import *
